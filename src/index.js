c8dc78b35d0f6d00055ed1e4e9dafd75dd75a665
Marcio Marçal da Costa Garcia <marciomarcal.dev@gmail.com>
2022-03-15 12:37:37 -0300

    Todos os testes passando e middlewares funcionando

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: 'Usuário não encontrado.' })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(user.todos.length < 10 || user.pro === true) {
+    return next()
+  }
+
+  return response.status(403).json({ error: 'Você já utilizou o máximo de to dos disponível. Atualize para PRO para criar todos ilimitados.' })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: 'Usuário não encontrado.' })
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({ error: 'Id inválido.' })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(404).json({ error: 'To do não encontrada.' })
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user) {
+    return response.status(404).json({ error: 'Usuário não encontrado.' })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
